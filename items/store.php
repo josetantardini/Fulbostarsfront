6601a92ba122c3754c67aad96d905e605ff2d982
josetantardini <JoseIgnacio.Tantardini@alumnos.uai.edu.ar>
2022-02-11 15:44:39 -0300

    ACTUALIZACION 11/02/2022

--- items/store.php
+++ items/store.php
@@ -15,19 +15,43 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
-   
+
 <a href="../index">Volver</a>
 <h1 style="text-align: center; margin-bottom:80px;">Items</h1>
 
     <div class="contenedor">
-        <div class="item">
-            <h1>Item</h1>
-            <h2>0.5FUL</h2>
+        <div class="item"  style="background-color: #BDBDBD;" >
+            <h1>common items</h1>
+            <h2>20FUL</h2>
             <img src="./items/images.jpg" alt="Imagen item">
-            <button onclick="buyitems(2)">Buy</button>
-            <div style="margin-top:20px;" id="result"></div>
+            <button onclick="buyitems(1)">Buy</button>
+            <div style="margin-top:20px;" id="comun"></div>
         </div>
         
+        <div class="item" style="background-color: #00FF00;">
+            <h1>uncommon items</h1>
+            <h2>50FUL</h2>
+            <img src="./items/images.jpg" alt="Imagen item">
+            <button onclick="buyitems(2)">Buy</button>
+            <div style="margin-top:20px;" id="nocomun"></div>
+        </div>
+
+        <div class="item" style="background-color: #006CFF;">
+            <h1>rare items</h1>
+            <h2>70FUL</h2>
+            <img src="./items/images.jpg" alt="Imagen item">
+            <button onclick="buyitems(3)">Buy</button>
+            <div style="margin-top:20px;" id="raro"></div>
+        </div>
+
+        <div class="item" style="background-color: #A200FF;">
+            <h1>legendary items</h1>
+            <h2>110FUL</h2>
+            <img src="./items/images.jpg" alt="Imagen item">
+            <button onclick="buyitems(4)">Buy</button>
+            <div style="margin-top:20px;" id="legendario"></div>
+          
+        </div>
 
     </div>
 
@@ -35,13 +59,17 @@
 
     
     <script>
- function buyitems(id){
-$(document).ready(function() {
 
+
+async function buyitems(id){
+
+let confirmacion = confirm('accept to make the purchase or cancel to not make the purchase, once purchased the items will be added to your inventory')
  
+
+if(confirmacion == true){
     var iditem = id;
   
-
+if(iditem == 1){
 
     $.ajax({
             type: 'POST',
@@ -53,17 +81,88 @@ $(document).ready(function() {
 
             },
             beforeSend: function() {
-                $('#result').html('...')
+                $('#comun').html('...')
             }
         })
         .done(function(response) {
-            $('#result').html(response)
+            $('#comun').html(response)
 
         })
 
-      
-})
+    }      
+
+    if(iditem == 2){
+
+$.ajax({
+        type: 'POST',
+        url: 'funciones/buyitems.php',
+        data: {
+            'id': iditem
+            
+
+
+        },
+        beforeSend: function() {
+            $('#nocomun').html('...')
+        }
+    })
+    .done(function(response) {
+        $('#nocomun').html(response)
+
+    })
+
+}  
+
+if(iditem == 3){
+
+$.ajax({
+        type: 'POST',
+        url: 'funciones/buyitems.php',
+        data: {
+            'id': iditem
+            
+
+
+        },
+        beforeSend: function() {
+            $('#raro').html('...')
+        }
+    })
+    .done(function(response) {
+        $('#raro').html(response)
+
+    })
+
+}  
+
+if(iditem == 4){
+
+$.ajax({
+        type: 'POST',
+        url: 'funciones/buyitems.php',
+        data: {
+            'id': iditem
+            
+
+
+        },
+        beforeSend: function() {
+            $('#legendario').html('...')
+        }
+    })
+    .done(function(response) {
+        $('#legendario').html(response)
+
+    })
+
+}  
+
+}
  }
+
+
+
+
 
 </script>
 
